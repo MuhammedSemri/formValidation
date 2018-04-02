ae0689acf90054be8a5e19832bc2a054dc8c0c61
muhammedsemri <muhammed.semri@gmail.com>
2018-04-02 19:07:18 +0300

    added alphanumeric validation

--- formval.ts
+++ formval.ts
@@ -4,10 +4,15 @@ export namespace FormValidation{
     
     // returns true if email is valid
     // false if not
-    export function isEmailValid(email: string){
+    export function isEmailValid(email: string):boolean {
         let reg = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
         return reg.test(email);
     }
 
-    
+    // returns true if string contains alphanumric characters only(alphabet,integers)
+    export function isAlphanumeric(str: string):boolean {
+        let reg = new RegExp(/^[a-z0-9]+$/i);
+        return reg.test(str);
+    }
+
 }
